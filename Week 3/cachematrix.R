47dbcf39fff3c447f241a0300d543beb2c915bd3
Andy Lassiter <festivus@vt.edu>
2014-10-25 18:31:09 -0400

    Project 2: First commit of cachematrix.

---
+++ Week 3/cachematrix.R
@@ -0,0 +1,32 @@
+## Put comments here that give an overall description of what your
+## functions do
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) inverse <<- inv 
+  getInverse <- function() inverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
+}
+
+## Write a short comment describing this function
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, diag(nrow(data)))
+  x$setInverse(inv)
+  inv
+}
